698043be873063f84ae562f475a802c295afbb0f
Ojash Gurung <OJASGGG@GMAIL.COM>
2023-02-05 00:37:34 +0545

    Social fixed design added at the side of entire design

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { BsGithub, BsLinkedin, BsTwitter } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+import { Link } from "react-router-dom";
 import {
   Navbar,
   Hero,
@@ -8,21 +11,64 @@ import {
   Contact,
 } from "./components";
 
+type socialItems = {
+  iconName: string;
+  logo: React.ReactElement<JSX.Element>;
+  link: string;
+};
+
+const socialItems: socialItems[] = [
+  {
+    iconName: "Github",
+    logo: <BsGithub size={18} />,
+    link: "https://github.com/ojasggg",
+  },
+  {
+    iconName: "Linkedin",
+    logo: <BsLinkedin size={18} />,
+    link: "https://www.linkedin.com/in/ojash-gurung",
+  },
+  {
+    iconName: "Twitter",
+    logo: <BsTwitter size={18} />,
+    link: "https://twitter.com/codeOjash",
+  },
+  {
+    iconName: "Email",
+    logo: <MdEmail size={20} />,
+    link: "#",
+  },
+];
+
 const App = () => {
+  // TODO: Mobile Responsive left
+  // TODO: Animation Left
   return (
-    <>
-      <div className="m-auto w-[1680px] bg-black">
+    <div>
+      <div className="h-[100vh]">
         <Navbar />
         <Hero />
-        <AboutMe />
-        <Project />
-        <ExperienceAndEducation />
-        <Skills />
       </div>
-      <div className="bg-[#D3D2D0]">
-        <Contact />
+      <AboutMe />
+      <Project />
+      <ExperienceAndEducation />
+      <Skills />
+      <Contact />
+      <div className="fixed bottom-0 lg:right-[70px] xl:right-[150px]">
+        <div className="flex flex-col items-center gap-8">
+          <ul className="flex flex-col items-center gap-[30px] text-white/60">
+            {socialItems.map((socialItem, index) => (
+              <li key={index}>
+                <Link to={socialItem.link}>
+                  {socialItem.logo && socialItem.logo}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div className="h-24 w-[2px] rounded-full bg-white"></div>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
