1b94c0da213af4e87b0f97be673dbd1ad73e874f
Ojash Gurung <OJASGGG@GMAIL.COM>
2023-02-08 19:30:55 +0545

    Contact Footer Opacity Maintained

--- src/components/Contact/ContactFooter.tsx
+++ src/components/Contact/ContactFooter.tsx
@@ -3,11 +3,11 @@ import React from "react";
 const ContactFooter = () => {
   return (
     <div className="flex justify-between border-t border-black/40 py-4 xl:py-5">
-      <p className="font-AvenirMedium text-[16px] text-black/60">
+      <p className="font-AvenirMedium text-[16px] text-black">
         ojasggg@gmail.com
       </p>
       <div>
-        <p className="font-AvenirMedium text-[16px] text-black/60">
+        <p className="font-AvenirMedium text-[16px] text-black">
           &copy; 2023 Ojash Gurung
         </p>
       </div>
