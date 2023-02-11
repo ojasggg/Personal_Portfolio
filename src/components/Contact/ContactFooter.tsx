d04d96c6bcc38c5aceb740df37d21a51ae513020
Ojash Gurung <OJASGGG@GMAIL.COM>
2023-02-11 23:33:01 +0545

    Added socials in footer rather than fixed right side in small screens

--- src/components/Contact/ContactFooter.tsx
+++ src/components/Contact/ContactFooter.tsx
@@ -1,13 +1,58 @@
 import React from "react";
+import { BsGithub, BsLinkedin, BsTwitter } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+type socialItems = {
+  iconName: string;
+  logo: React.ReactElement<JSX.Element>;
+  link: string;
+}[];
+
+const socialItems: socialItems = [
+  {
+    iconName: "Github",
+    logo: <BsGithub size={14} />,
+    link: "https://github.com/ojasggg",
+  },
+  {
+    iconName: "Linkedin",
+    logo: <BsLinkedin size={14} />,
+    link: "https://www.linkedin.com/in/ojash-gurung",
+  },
+  {
+    iconName: "Twitter",
+    logo: <BsTwitter size={14} />,
+    link: "https://twitter.com/codeOjash",
+  },
+  {
+    iconName: "Email",
+    logo: <MdEmail size={16} />,
+    link: "#",
+  },
+];
 
 const ContactFooter = () => {
   return (
-    <div className="flex justify-between border-t border-black/40 py-4 xl:py-5">
-      <p className="font-AvenirMedium text-[16px] text-black">
+    <div className="m-auto mt-10 flex w-[90%] justify-between border-t border-black/40 py-4 pb-4  lg:m-0 lg:w-full lg:pb-0 xl:py-5">
+      <p className="hidden font-AvenirMedium text-[16px] text-black lg:flex">
         ojasggg@gmail.com
       </p>
+      <ul className="flex items-center gap-[14px] text-black lg:hidden">
+        {socialItems.map((socialItem, index) => (
+          <li
+            key={index}
+            className="transition-all duration-150 ease-in-out hover:-translate-y-1 hover:scale-105 hover:text-white"
+          >
+            <Link to={socialItem.link} target="_blank">
+              {socialItem.logo && socialItem.logo}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
       <div>
-        <p className="font-AvenirMedium text-[16px] text-black">
+        <p className="font-AvenirMedium text-[14px] text-black lg:text-[16px]">
           &copy; 2023 Ojash Gurung
         </p>
       </div>
