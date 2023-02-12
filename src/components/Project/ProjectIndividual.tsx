f8e1857624518707438b02f985f3c3928bc0bfb0
Ojash Gurung <OJASGGG@GMAIL.COM>
2023-02-12 23:30:41 +0545

    Animation on Project page done

--- src/components/Project/ProjectIndividual.tsx
+++ src/components/Project/ProjectIndividual.tsx
@@ -51,29 +51,62 @@ const ProjectIndividual = ({ projectId }: { projectId: string }) => {
       </div>
       <div>
         <div className="mx-auto w-[95%] py-8">
-          <h4 className="border-b border-white pb-4 font-AvenirMedium text-[24px] leading-none">
+          <h4
+            className="border-b border-white pb-4 font-AvenirMedium text-[24px] leading-none"
+            data-aos="fade-left"
+            data-aos-delay="200"
+            data-aos-duration="1000"
+          >
             About this project
           </h4>
-          <p className="mt-4 font-AvenirBook text-[18px] text-white/60">
+          <p
+            className="mt-4 font-AvenirBook text-[18px] text-white/60"
+            data-aos="fade-right"
+            data-aos-delay="300"
+            data-aos-duration="1000"
+          >
             {Project?.projectDesc}
           </p>
         </div>
         <div className="mx-auto w-[95%] py-4">
-          <h6 className="border-b border-white pb-4 font-AvenirMedium text-[20px] leading-none">
+          <h6
+            className="border-b border-white pb-4 font-AvenirMedium text-[20px] leading-none"
+            data-aos="fade-left"
+            data-aos-delay="200"
+            data-aos-duration="1000"
+          >
             Role in project
           </h6>
           <ul className="mt-4 list-inside list-disc font-AvenirBook text-[18px] text-white/60">
-            <li>{Project?.projectMyRole}</li>
+            <li
+              data-aos="fade-down"
+              data-aos-delay="300"
+              data-aos-duration="1000"
+            >
+              {Project?.projectMyRole}
+            </li>
           </ul>
         </div>
 
         <div className="mx-auto w-[95%] py-4">
-          <h4 className="border-b border-white pb-4 font-AvenirMedium text-[20px]">
+          <h4
+            className="border-b border-white pb-4 font-AvenirMedium text-[20px]"
+            data-aos="fade-left"
+            data-aos-delay="200"
+            data-aos-duration="1000"
+          >
             Technical Sheet
           </h4>
           <ul className="mt-4 flex list-inside list-disc flex-col gap-3 font-AvenirBook text-[18px] text-white/60">
             {Project?.projectTechnicalSheets?.map((tech, index) => (
-              <li key={index}>{tech}</li>
+              <li
+                key={index}
+                data-aos="fade-down"
+                data-aos-delay={`${200 + index * 100}`}
+                data-aos-duration="1000"
+              >
+                {tech}
+              </li>
             ))}
           </ul>
         </div>
