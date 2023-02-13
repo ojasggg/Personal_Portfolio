aba394fece9293bde57374ab7668b9297eb64ddb
Ojash Gurung <OJASGGG@GMAIL.COM>
2023-02-13 17:19:29 +0545

    Changed name of 404 page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,4 +3,4 @@ export { default as Projects } from "./Projects";
 export { default as ProjectPage } from "./ProjectPage";
 export { default as SiteCredit } from "./SiteCredit";
 export { default as About } from "./About";
-export { default as Error } from "./Error";
+export { default as Error } from "./404";
