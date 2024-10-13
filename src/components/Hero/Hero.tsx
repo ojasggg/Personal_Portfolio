9988d89979284b44c03306206bfd151518f35370
Ojash Gurung <ojasggg@gmail.com>
2024-10-13 19:10:45 -0400

    Upgrading the Hero section for mobile devices

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -16,21 +16,29 @@ const Hero = () => {
         data-aos-delay="600"
       /> */}
       <h1
-        className="mb-[46px] py-0 text-center font-Valencia text-[120px] leading-none text-white lg:text-[140px]"
+        className="sm:mt-[300px] lg:mt-0 mb-[46px] py-0 text-center font-Valencia text-[100px] leading-none text-white lg:text-[140px]"
         data-aos="fade-up"
         data-aos-delay="600"
       >
         Ojash Gurung
       </h1>
       <h3
-        className="w-[50%] text-center font-AvenirRoman text-[24px] lg:text-[28px]"
+        className="w-[50%] text-center font-AvenirRoman text-[22px] lg:text-[28px]"
         data-aos="fade-up"
         data-aos-delay="700"
       >
         <span className="leading-relaxed">
-          MSCS Student at Monroe University <span className="text-sm">(Expected Graduation, Sep 2026)</span><br></br>| Software Development | Artificial Intelligence |<br></br>| SpringBoot | React.js |
-        </span>
+          MSCS Student at Monroe University <span className="text-[12px] lg:text-sm">(Expected Graduation, Sep 2026)</span><br></br></span>
       </h3>
+      <h3
+        className="w-[80%] mt-4 text-center font-AvenirRoman text-[22px] lg:text-[28px] leading-relaxed"
+        data-aos="fade-up"
+        data-aos-delay="700"
+      >
+        <span>| Software Development | Artificial Intelligence |<br></br>| Python | Rest Framework |</span>
+      </h3>
+      
+        
     </section>
   );
 };
