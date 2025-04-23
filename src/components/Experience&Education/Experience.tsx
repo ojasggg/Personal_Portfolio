959e923db3e8eeb2bcf10c3a067ca4f884b6062c
Ojash Gurung <ojasggg@gmail.com>
2025-04-23 18:48:19 -0400

    refactor(Experience&Education): restructure components to use external data sources
    Move education and experience data to constants files for better maintainability and reusability. Update component layouts and styles for consistency and improved readability.

--- src/components/Experience&Education/Experience.tsx
+++ src/components/Experience&Education/Experience.tsx
@@ -1,132 +1,101 @@
 import { BsArrowUpRight } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { experience } from "../../constants/experience";
 
 const Experience = () => {
   return (
-    <div className="relative">
+    <section>
       <h1
-        className="absolute top-10 left-0 z-[-1] p-0 font-AvenirHeavy text-[70px] uppercase leading-none tracking-tighter text-white/20 lg:text-[160px] xl:text-[200px] xl:text-white/10"
+        className="font-AvenirHeavy text-[70px] uppercase leading-none tracking-tighter text-white/20 lg:text-[160px] xl:text-[200px] xl:text-white/10"
         data-aos="fade-left"
         data-aos-delay="200"
         data-aos-duration="1200"
       >
         Experience
       </h1>
-      <div className="z-[10] m-auto w-[90%] pt-[160px] lg:ml-[142px] lg:pt-[160px]">
-        <p
-          className="font-AvenirBook text-[14px] text-white/60 lg:text-[18px]"
-          data-aos="fade-left"
-          data-aos-delay="300"
-          data-aos-duration="800"
+      {experience.map((exp, index) => (
+        <div
+          key={index}
+          className="z-[10] m-auto w-[90%] pt-[160px] first:pt-[160px] lg:ml-[142px] lg:pt-[60px]"
         >
-          Apr 2024 – May 2024
-        </p>
-        <div className="flex items-center gap-2 lg:gap-4 lg:pt-2">
-          <h3
-            className="z-10 p-0 font-AvenirMedium text-[16px] uppercase leading-none text-white lg:text-[26px] xl:text-[30px]"
-            data-aos="fade-left"
-            data-aos-delay="400"
-            data-aos-duration="800"
-          >
-            Computer Lab Assistant (Part Time)
-          </h3>
-          <div
-            className="h-[2px] w-10 rounded-full bg-white xl:h-1 "
-            data-aos="fade-left"
-            data-aos-delay="400"
-            data-aos-duration="800"
-          ></div>
           <p
-            className="z-10 p-0 font-AvenirLight text-[16px] leading-none text-white/60 lg:text-[18px] xl:text-[20px] uppercase"
+            className="font-AvenirBook text-[14px] text-white/60 lg:text-[18px]"
             data-aos="fade-left"
-            data-aos-delay="400"
+            data-aos-delay="300"
             data-aos-duration="800"
           >
-            Troy University
+            {exp.duration}
           </p>
-        </div>
-        <div className="mt-4">
-        <p
-            className="leading-wide max-w-[1040px] font-AvenirRoman text-[16px] text-white/60 lg:text-[20px] xl:text-[24px] xl:leading-10"
-            data-aos="fade-up"
-            data-aos-delay="200"
-            data-aos-duration="1000"
-          >
-            ● Led lab sessions for 100+ students in the Introduction to Programming with Python course, providing guidance on coding concepts, debugging, and effective problem-solving techniques 
-          </p>
-          <p
-            className="leading-wide max-w-[1040px] font-AvenirRoman text-[16px] text-white/60 lg:text-[20px] xl:text-[24px] xl:leading-10 mt-2"
-            data-aos="fade-up"
-            data-aos-delay="200"
-            data-aos-duration="1000"
-          >
-            ● Assisted students enrolled in CS-2250 - Computer Science I with their final projects, offering technical support, debugging assistance, and feedback to improve project outcomes
-          </p>
-          <p
-            className="leading-wide max-w-[1040px] font-AvenirRoman text-[16px] text-white/60 lg:text-[20px] xl:text-[24px] xl:leading-10 mt-2"
-            data-aos="fade-up"
-            data-aos-delay="200"
-            data-aos-duration="1000"
-          >
-            ● Installed and configured operating systems and software across 30+ computers per semester, ensuring smooth lab operations for over 300 users
-          </p>
-        </div>
-      </div>
-      <div className="z-[10] m-auto w-[90%] pt-[160px] lg:ml-[142px] lg:pt-[60px]">
-        <p
-          className="font-AvenirBook text-[14px] text-white/60 lg:text-[18px]"
-          data-aos="fade-left"
-          data-aos-delay="300"
-          data-aos-duration="800"
-        >
-          Dec 2021 – April 2022
-        </p>
-        <div className="flex items-center gap-2 lg:gap-4 lg:pt-2">
-          <h3
-            className="z-10 p-0 font-AvenirMedium text-[16px] uppercase leading-none text-white lg:text-[26px] xl:text-[30px]"
-            data-aos="fade-left"
-            data-aos-delay="400"
-            data-aos-duration="800"
-          >
-            Teaching Assistant
-          </h3>
-          <div
-            className="h-[2px] w-10 rounded-full bg-white xl:h-1 "
-            data-aos="fade-left"
-            data-aos-delay="400"
-            data-aos-duration="800"
-          ></div>
-          <p
-            className="z-10 p-0 font-AvenirLight text-[16px] leading-none text-white/60 lg:text-[18px] xl:text-[20px] uppercase"
-            data-aos="fade-left"
-            data-aos-delay="400"
-            data-aos-duration="800"
-          >
-            Coventry University (Softwarica College)
-          </p>
-        </div>
-        <div className="mt-4">
-          <p
-            className="leading-wide max-w-[1040px] font-AvenirRoman text-[16px] text-white/60 lg:text-[20px] xl:text-[24px] xl:leading-10"
-            data-aos="fade-up"
-            data-aos-delay="200"
-            data-aos-duration="1000"
-          >
-            ● Graded weekly quizzes for 75+ students in the Developing the Modern Web course (STW205CDE), evaluating knowledge in React.js, REST, and Node.js, and ensuring timely feedback to improve learning outcomes by 15%
+          <div className="flex items-center gap-2 lg:gap-4 lg:pt-2">
+            <h3
+              className="z-10 p-0 font-AvenirMedium text-[16px] uppercase leading-none text-white lg:text-[26px] xl:text-[30px]"
+              data-aos="fade-left"
+              data-aos-delay="400"
+              data-aos-duration="800"
+            >
+              {exp.role}
+            </h3>
+            <div
+              className="h-[2px] w-10 rounded-full bg-white xl:h-1"
+              data-aos="fade-left"
+              data-aos-delay="400"
+              data-aos-duration="800"
+            ></div>
 
-          </p>
-          <p
-            className="leading-wide max-w-[1040px] font-AvenirRoman text-[16px] text-white/60 lg:text-[20px] xl:text-[24px] xl:leading-10 mt-4"
-            data-aos="fade-up"
-            data-aos-delay="200"
-            data-aos-duration="1000"
-          >
-            ● Provided one-on-one virtual support to 40+ students, assisting with final assignment submissions, topic
-            selection, and troubleshooting technical challenges, leading to a 95% project completion rate
-          </p>
+            {exp.link ? (
+              <Link
+                to={exp.link}
+                target="_blank"
+                className="flex items-center gap-2 text-white/60 transition-colors duration-200 hover:text-white"
+                data-aos="fade-left"
+                data-aos-delay="400"
+                data-aos-duration="800"
+              >
+                <p
+                  className="z-10 p-0 font-AvenirLight text-[16px] uppercase leading-none lg:text-[18px] xl:text-[20px]"
+                  data-aos="fade-left"
+                  data-aos-delay="400"
+                  data-aos-duration="800"
+                >
+                  {exp.company}
+                </p>
+                <BsArrowUpRight size={20} />
+              </Link>
+            ) : (
+              <p
+                className="z-10 p-0 font-AvenirLight text-[16px] uppercase leading-none text-white/60 lg:text-[18px] xl:text-[20px]"
+                data-aos="fade-left"
+                data-aos-delay="400"
+                data-aos-duration="800"
+              >
+                {exp.company}
+              </p>
+            )}
+          </div>
+          <div className="mt-4">
+            <p
+              className="leading-wide mb-4 max-w-[1040px] font-AvenirRoman text-[16px] text-white/60 lg:text-[20px] xl:text-[24px] xl:leading-10"
+              data-aos="fade-up"
+              data-aos-delay="200"
+              data-aos-duration="1000"
+            >
+              {exp.description}
+            </p>
+            {exp.highlights.map((highlight, hIndex) => (
+              <p
+                key={hIndex}
+                className="leading-wide mt-2 max-w-[1040px] font-AvenirRoman text-[16px] text-white/60 lg:text-lg xl:text-[24px] xl:leading-10"
+                data-aos="fade-up"
+                data-aos-delay="200"
+                data-aos-duration="1000"
+              >
+                ● {highlight}
+              </p>
+            ))}
+          </div>
         </div>
-      </div>
-    </div>
+      ))}
+    </section>
   );
 };
 
